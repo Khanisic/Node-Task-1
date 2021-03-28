d94db0bcc10d7e5ce4daa7ffb2decdd5e757d1fa
Khanisic <62929300+Khanisic@users.noreply.github.com>
2021-03-28 17:18:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 var content = "";
+//Putting the meta tag to distinctively identify files as directory or file with the help of emojis
 content = `<meta charset="UTF-8">`
 let foo = (err, files) => {
         if (err) throw err;
@@ -9,8 +10,10 @@ let foo = (err, files) => {
         for (i = 0; i < fl; i++) {
             var stats = fs.statSync(`C:/Users/Khanisic/Desktop/${files[i]}`);
             if (stats.isFile()) {
+                    //If it is as file
                 content+=`<p> &#128221; ${files[i]}</p>`;
             } else {
+                    //If it is a directory
                 content+=`<p>&#128213; ${files[i]}</p>`;
             }
         } 
